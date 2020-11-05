28f9fa41424804fd298732a26f499fcfc8cd225b
Juanes <jedp082@gmail.com>
2020-11-04 21:28:08 -0500

    fixed resolution of UI

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Animations : MonoBehaviour
+{
+
+    public GameObject menu;
+    public GameObject help;
+
+    private void Start()
+    {
+        help.GetComponent<Animator>().SetBool("isShowMenu", false);
+    }
+
+    public void showMenu()
+    {
+        menu.GetComponent<Animator>().SetBool("isShowMenu", true);
+    }
+}
