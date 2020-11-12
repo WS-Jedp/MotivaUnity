2c0f533073bdb3b663a9fc9f5382c7b2ffd531b0
Juanes <jedp082@gmail.com>
2020-11-12 15:53:36 -0500

    Missions with medals scene

---
+++ Assets/Scripts/UnlockTrophy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockTrophy : MonoBehaviour
+{
+    public GameObject[] Medals;
+
+    // The current mission of the application
+    private int currentMission = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= currentMission; i++)
+        {
+            Medals[i].GetComponent<Animator>().SetBool("firstMission", true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
